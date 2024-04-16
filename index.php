ccb5f0d769fdb9e375fa9e6388db9ce751af45fb
andriyani6 <152153346+andriyani6@users.noreply.github.com>
2024-04-17 05:02:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,266 @@
+<!--================Home Banner Area =================-->
+<section class="home_banner_area">
+	<div class="banner_inner d-flex align-items-center">
+		<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
+		</div>
+		<div class="container">
+			<div class="banner_content text-center">
+				<h3 data-aos="fade-up" data-aos-duration="1600">Learn Anywhere & Anytime <br /> Easy with Learning</h3>
+				<p data-aos="fade-up" data-aos-duration="1900">With Learning, the ease of teaching and learning activities can be fulfilled. Teachers and students can
+learn despite many obstacles or obstacles. Enjoy structured and effective learning using Learning as well as ease of learning using our application. </p>
+				<a data-aos="fade-up" data-aos-duration="2000" class="main_btn" href="<?= base_url('user/registration') ?>#registration">Joint Now <span class="lnr lnr-arrow-right"></span></a>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- ================End Home Banner Area ================= -->
+
+<!--================Finance Area =================-->
+<section class="finance_area">
+	<div class="container">
+		<div class="finance_inner row">
+			<div class="col-lg-3 col-sm-6">
+				<div class="finance_item">
+					<div data-aos="fade-right" data-aos-duration="1600" class="media">
+						<div class="d-flex">
+							<i class="lnr lnr-users"></i>
+						</div>
+						<div class="media-body">
+							<h5>Admin, User & Teacher Data Management</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6">
+				<div class="finance_item">
+					<div data-aos="fade-right" data-aos-duration="1800" class="media">
+						<div class="d-flex">
+							<i class="lnr lnr-file-empty"></i>
+						</div>
+						<div class="media-body">
+							<h5>Complete & Clear Documentation</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div data-aos="fade-right" data-aos-duration="2000" class="col-lg-3 col-sm-6">
+				<div class="finance_item">
+					<div class="media">
+						<div class="d-flex">
+							<i class="lnr lnr-camera-video"></i>
+						</div>
+						<div class="media-body">
+							<h5>E-Learning Berbasis Video and Discus</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div data-aos="fade-right" data-aos-duration="2200" class="col-lg-3 col-sm-6">
+				<div class="finance_item">
+					<div class="media">
+						<div class="d-flex">
+							<i class="lnr lnr-tag"></i>
+						</div>
+						<div class="media-body">
+							<h5>Free with MIT Open-Source License</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================End Finance Area =================-->
+
+<!--================ Illustrations Area =================-->
+<section class="learnify-for-indonesia p_20">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 mx-auto">
+				<img data-aos="fade-up" data-aos-duration="1800" src="<?= base_url('assets/') ?>img/illustrations/index-study.svg" alt="" srcset="">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-7 mx-auto">
+				<div class="main_title">
+					<h2 data-aos="fade-up" data-aos-duration="2000">Learning was created to improve the quality of learning in Indonesia</h2>
+					<p data-aos="fade-up" data-aos-duration="2200">Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free.</p>
+					<a href="https://github.com/syauqi/learnify"><button data-aos="fade-up" data-aos-duration="2400" class="bubbly-button">Download Learning <span class="lnr lnr-arrow-right"></span></button></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================End Illustrations Area =================-->
+
+<!--================Courses Area =================-->
+<section class="courses_area p_40">
+	<div class="container">
+		<div class="main_title">
+			<h2 data-aos="fade-up" data-aos-duration="1600">Lessons Available on Learning</h2>
+			<p data-aos="fade-up" data-aos-duration="1800">Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free.</p>
+		</div>
+		<div class="row courses_inner">
+			<div class="col-lg-9">
+				<div class="grid_inner">
+					<div class="grid_item wd55">
+						<div class="courses_item" data-aos="fade-right" data-aos-duration="1800">
+							<img src="<?= base_url('assets/') ?>img/courses/course-1.jpg" alt="">
+							<div class="hover_text">
+								<a class="cat" href="#">Free</a>
+								<a href="javaScript:void(0);">
+									<h4>Free Math Classes</h4>
+								</a>
+								<ul class="list">
+									<li><a href="#"><i class="lnr lnr-users"></i>54</a></li>
+									<li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+									<li><a href="#"><i class="lnr lnr-user"></i>Mathematics Teacher at SMAN 1 BANYUMAS</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="grid_item wd44">
+						<div class="courses_item" data-aos="fade-down" data-aos-duration="1800">
+							<img src="<?= base_url('assets/') ?>img/courses/course-2.jpg" alt="">
+							<div class="hover_text">
+								<a class="cat" href="#">free</a>
+								<a href="javaScript:void(0);">
+									<h4>Free Science Classes</h4>
+								</a>
+								<ul class="list">
+									<li><a href="#"><i class="lnr lnr-users"></i> 34</a></li>
+									<li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+									<li><a href="#"><i class="lnr lnr-user"></i> Teacher at SMAN 1 BANYUMAS</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="grid_item wd44">
+						<div class="courses_item" data-aos="fade-right" data-aos-duration="1800">
+							<img src="<?= base_url('assets/') ?>img/courses/course-4.jpg" alt="">
+							<div class="hover_text">
+								<a class="cat" href="#">Free</a>
+								<a href="javaScript:void(0);">
+									<h4>Free English Classes</h4>
+								</a>
+								<ul class="list">
+									<li><a href="#"><i class="lnr lnr-users"></i> 63</a></li>
+									<li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+									<li><a href="#"><i class="lnr lnr-user"></i> English teacher at SMAN 1 BANYUMAS</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="grid_item wd55">
+						<div class="courses_item" data-aos="fade-up" data-aos-duration="1800">
+							<img src="<?= base_url('assets/') ?>img/courses/course-5.jpg" alt="">
+							<div class="hover_text">
+								<a class="cat" href="#">Free</a>
+								<a href="javaScript:void(0);">
+									<h4>Free Indonesian Language Class</h4>
+								</a>
+								<ul class="list">
+									<li><a href="#"><i class="lnr lnr-users"></i> 24</a></li>
+									<li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+									<li><a href="#"><i class="lnr lnr-user"></i> Indonesian teacher at SMAN 1 BANYUMAS</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="course_item" data-aos="fade-left" data-aos-duration="1800">
+					<img src="<?= base_url('assets/') ?>img/courses/course-3.jpg" alt="">
+					<div class="hover_text">
+						<a class="cat" href="#">Free</a>
+						<a href="javaScript:void(0);">
+							<h4>Free Islamic Religious Education Classes</h4>
+						</a>
+						<ul class="list">
+							<li><a href="#"><i class="lnr lnr-users"></i> 35</a></li>
+							<li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+							<li><a href="#"><i class="lnr lnr-user"></i> Religion Teacher at SMAN 1 BANYUMAS</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================End Courses Area =================-->
+
+<!--================Team Area =================-->
+<section class="team_area p_20">
+	<div class="container">
+		<div class="main_title">
+			<h2 data-aos="fade-up" data-aos-duration="1800">Testimonials from Learning Students</h2>
+			<p data-aos="fade-up" data-aos-duration="2000">Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free.</p>
+		</div>
+		<section class="testimonials_area p_20">
+			<div class="container">
+				<div class="testi_slider owl-carousel">
+					<div class="item">
+						<div class="testi_item">
+							<img src="<?= base_url('assets/') ?>img/testimonials/testi-3.png" alt="">
+							<h4>Andriyani</h4>
+							<ul class="list">
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+							</ul>
+							<p>Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free. Abstract chaos snare play truth ultimate good self. God overcome sexuality pious abstract good decieve revaluation aversion good. Virtues chaos overcome society holiest truth.</p>
+						</div>
+					</div>
+					<div class="item">
+						<div class="testi_item">
+							<img src="<?= base_url('assets/') ?>img/testimonials/testi-2.png" alt="">
+							<h4>Upik Hidayat</h4>
+							<ul class="list">
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+							</ul>
+							<p>Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free. Abstract chaos snare play truth ultimate good self. God overcome sexuality pious abstract good decieve revaluation aversion good. Virtues chaos overcome society holiest truth.</p>
+						</div>
+					</div>
+					<div class="item">
+						<div class="testi_item">
+							<img src="<?= base_url('assets/') ?>img/testimonials/testi-1.png" alt="">
+							<h4>Diki Ramdani</h4>
+							<ul class="list">
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+							</ul>
+							<p>Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free. Abstract chaos snare play truth ultimate good self. God overcome sexuality pious abstract good decieve revaluation aversion good. Virtues chaos overcome society holiest truth.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</section>
+<!--================End Team Area =================-->
+
+<!--================Impress Area =================-->
+<section class="impress_area p_120">
+	<div class="container">
+		<div class="impress_inner text-center">
+			<h2 data-aos="fade-up" data-aos-duration="1800">LOGIN AS A TEACHER AND UPLOAD MATERIALS & VIDEOS NOW</h2>
+			<p data-aos="fade-up" data-aos-duration="2000">Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free. Abstract chaos snare play truth ultimate good self. God overcome sexuality pious abstract good decieve revaluation aversion good. Virtues chaos overcome society holiest truth.
+			</p>
+			<a data-aos="fade-up" data-aos-duration="2200" class="main_btn" href="<?= base_url('welcome/guru') ?>">Login As Teacher <span class="lnr lnr-arrow-right text-black"></span></a>
+		</div>
+	</div>
+</section>
+<!--================End Impress Area =================-->
